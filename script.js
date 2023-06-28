99fbe45c1ab81db8e4d8e1a69c007176617d24aa
adityaK006 <120311231+adityaK006@users.noreply.github.com>
2023-06-29 01:43:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-    // Initialize Firebase configuration
-    const firebaseConfig = {
-
-        apiKey: "AIzaSyAvBc_a5C8QkjZyBFf3YcT7RjpPSdolj88",
-      
-        authDomain: "park-f8eef.firebaseapp.com",
-      
-        databaseURL: "https://park-f8eef-default-rtdb.asia-southeast1.firebasedatabase.app",
-      
-        projectId: "park-f8eef",
-      
-        storageBucket: "park-f8eef.appspot.com",
-      
-        messagingSenderId: "293760294853",
-      
-        appId: "1:293760294853:web:c817437449947d13d0f5ac",
-      
-        measurementId: "G-26J5QN1Y1Y"
-      
-      };
-          // Initialize Firebase
-          firebase.initializeApp(firebaseConfig);
-      
-          // Get references to Firebase services
-          const database = firebase.database();
-          const firestore = firebase.firestore();
-          const auth = firebase.auth();
-      
-const emptySlotsCountElement = document.querySelector('.empty-slots-count');
-const parkingSlotsTab = document.getElementById('parkingSlotsTab');
-const manageSlotsTab = document.getElementById('manageSlotsTab');
-const manageSlotsContainer = document.getElementById('manageSlotsContainer');
-
 let emptySlotsCount = 30;
 const totalSlots = 30;
 const bookedSlots = [];
