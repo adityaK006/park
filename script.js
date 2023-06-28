39f857c31f798c1d7e6bfd8bdaf9e876f88eb100
adityaK006 <120311231+adityaK006@users.noreply.github.com>
2023-06-29 00:22:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+    // Initialize Firebase configuration
+    const firebaseConfig = {
+
+        apiKey: "AIzaSyAvBc_a5C8QkjZyBFf3YcT7RjpPSdolj88",
+      
+        authDomain: "park-f8eef.firebaseapp.com",
+      
+        databaseURL: "https://park-f8eef-default-rtdb.asia-southeast1.firebasedatabase.app",
+      
+        projectId: "park-f8eef",
+      
+        storageBucket: "park-f8eef.appspot.com",
+      
+        messagingSenderId: "293760294853",
+      
+        appId: "1:293760294853:web:c817437449947d13d0f5ac",
+      
+        measurementId: "G-26J5QN1Y1Y"
+      
+      };
+          // Initialize Firebase
+          firebase.initializeApp(firebaseConfig);
+      
+          // Get references to Firebase services
+          const database = firebase.database();
+          const firestore = firebase.firestore();
+          const auth = firebase.auth();
+      
 const emptySlotsCountElement = document.querySelector('.empty-slots-count');
 const parkingSlotsTab = document.getElementById('parkingSlotsTab');
 const manageSlotsTab = document.getElementById('manageSlotsTab');
